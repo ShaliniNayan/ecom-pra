390955e0d7bb61b11ec1e73e9712ad4de7832bc0
Shalini Nayan <shalininayan@outlook.com>
2024-09-19 20:46:49 +0530

    Add Autho Provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-m3bi1ymtp0nb7stu.us.auth0.com"
+    clientId="dkUEb886HjdQF0L9Ko0j9lVX8biCv480"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
