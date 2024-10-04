7c2d134b3f383da74c8e4147a36a467e3057aad9
Shalini Nayan <shalininayan@outlook.com>
2024-10-04 22:37:54 +0530

    Update the image and stylling in home page

--- src/comp/home.js
+++ src/comp/home.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import './nav.css'
+import React from 'react';
 
 const Home = () => {
   return (
-    <div className='home'>
-        <div className='top_banner'></div>
+    <div className="w-full">
+      <div 
+        className="w-full h-[500px] bg-cover bg-no-repeat" 
+        style={{ backgroundImage: "url('http://localhost:3000/image/slide-1.webp')" }}
+      >
+        {/* Add any content inside the banner here */}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
