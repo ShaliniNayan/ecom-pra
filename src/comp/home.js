3d136052fd1805e81489740579ddef7196d71488
Shalini Nayan <shalininayan@outlook.com>
2024-10-06 22:41:54 +0530

    Add link for shop now with styling

--- src/comp/home.js
+++ src/comp/home.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import './nav.css'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div className="w-full">
       <div 
-        className="w-full h-[395px] bg-cover bg-no-repeat" 
+        className="w-full h-[395px] p-[50px_60px] bg-cover bg-no-repeat" 
         style={{ backgroundImage: "url('http://localhost:3000/image/slide-1.webp')" }}
       >
         <h3>Silver Aluminum</h3>
         <h2>Apple Watch</h2>
         <p>30% OFF</p>
+        <Link to='/shop' className="w-full relative p-[10px_50px]" style={{top: '20%'}}>Shop Now</Link>
       </div>
     </div>
   );
