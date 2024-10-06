5ec3518c9946ecd74c6841a48e5ab16b92f80498
Shalini Nayan <shalininayan@outlook.com>
2024-10-06 22:46:59 +0530

    Update the content part

--- src/comp/home.js
+++ src/comp/home.js
@@ -9,10 +9,12 @@ const Home = () => {
         className="w-full h-[395px] p-[50px_60px] bg-cover bg-no-repeat" 
         style={{ backgroundImage: "url('http://localhost:3000/image/slide-1.webp')" }}
       >
-        <h3>Silver Aluminum</h3>
-        <h2>Apple Watch</h2>
-        <p>30% OFF</p>
-        <Link to='/shop' className="w-full relative p-[10px_50px]" style={{top: '20%'}}>Shop Now</Link>
+        <div className="w-full relative p-[10px_50px]" style={{top: '20%'}}>
+          <h3>Silver Aluminum</h3>
+          <h2>Apple Watch</h2>
+          <p>30% OFF</p>
+          <Link to='/shop'>Shop Now</Link>
+        </div>
       </div>
     </div>
   );
