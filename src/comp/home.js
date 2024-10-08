1d055c40f6b505a0c9cf2f876e37f9671c4c1f77
Shalini Nayan <shalininayan@outlook.com>
2024-10-08 22:20:42 +0530

    Update styling for h2

--- src/comp/home.js
+++ src/comp/home.js
@@ -11,8 +11,8 @@ const Home = () => {
       >
         <div className="w-full relative p-[10px_50px]" style={{top: '20%'}}>
           <h3 className="capitalize text-[#cd1e76] font-semibold tracking-wider">Silver Aluminum</h3>
-          <h2>Apple Watch</h2>
-          <p>30% OFF</p>
+          <h2 className="uppercase text-[#232323] font-semibold tracking-wider text-[42px] mt-2.5">Apple Watch</h2>
+          <p >30% OFF</p>
           <Link to='/shop'>Shop Now</Link>
         </div>
       </div>
