8d7c952092a10b14fab3bfec7f00a774c8913bc4
Shalini Nayan <shalininayan@outlook.com>
2024-10-08 22:41:33 +0530

    Update styling and fix the error for link and paragraph

--- src/comp/home.js
+++ src/comp/home.js
@@ -12,8 +12,13 @@ const Home = () => {
         <div className="w-full relative p-[10px_50px]" style={{top: '20%'}}>
           <h3 className="capitalize text-[#cd1e76] font-semibold tracking-wider">Silver Aluminum</h3>
           <h2 className="uppercase text-[#232323] font-semibold tracking-wider text-[42px] mt-2.5">Apple Watch</h2>
-          <p className='text-[#232323] tracking-wider mt-2.5 mb-7.5'>30% OFF</p>
-          <Link to='/shop'>Shop Now</Link>
+          <p className="text-[#232323] tracking-wider mt-2.5 mb-4">30% OFF</p>
+          <Link
+            to='/shop'
+            className="inline-block px-8 py-3 bg-[#fed700] text-[#232323] no-underline rounded-md transition duration-500 hover:text-[#fed700] hover:bg-[#cd1e76] mt-2"
+          >
+            Shop Now
+          </Link>
         </div>
       </div>
     </div>
