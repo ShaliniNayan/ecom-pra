e19c7d118bb4a798b99675bd6bb0bf5f3e71c5ca
Shalini Nayan <shalininayan@outlook.com>
2024-10-08 22:27:17 +0530

    Add margin bottom in paragraph

--- src/comp/home.js
+++ src/comp/home.js
@@ -12,7 +12,7 @@ const Home = () => {
         <div className="w-full relative p-[10px_50px]" style={{top: '20%'}}>
           <h3 className="capitalize text-[#cd1e76] font-semibold tracking-wider">Silver Aluminum</h3>
           <h2 className="uppercase text-[#232323] font-semibold tracking-wider text-[42px] mt-2.5">Apple Watch</h2>
-          <p className='text-[#232323] tracking-wider mt-2.5'>30% OFF</p>
+          <p className='text-[#232323] tracking-wider mt-2.5 mb-7.5'>30% OFF</p>
           <Link to='/shop'>Shop Now</Link>
         </div>
       </div>
