5419a2aa4db15ea73f19d0c8954f57108a0764a2
Shalini Nayan <shalininayan@outlook.com>
2024-10-18 00:20:35 +0530

    Try to fix the error

--- src/comp/home_product.js
+++ src/comp/home_product.js
@@ -1,76 +1,68 @@
-const Homeproduct =
-[
+const Homeproduct = [
     {
         id: 1,
         Name: "Product1",
         price: '100',
-        img: 'img/p1.webp',
+        image: 'img/p1.webp',
         cat: '',
         type: 'new',
     },
-
     {
         id: 2,
         Name: "Product2",
         price: '120',
-        img: 'img/p2.webp',
+        image: 'img/p2.webp',
         cat: '',
         type: 'new',
     },
-
     {
         id: 3,
         Name: "Product3",
         price: '140',
-        img: 'img/p3.webp',
+        image: 'img/p3.webp',
         cat: '',
         type: 'new',
     },
-
     {
         id: 4,
         Name: "Product4",
         price: '160',
-        img: 'img/p4.webp',
+        image: 'img/p4.webp',
         cat: '',
         type: 'new',
     },
-
     {
         id: 5,
         Name: "Product5",
         price: '180',
-        img: 'img/p5.webp',
+        image: 'img/p5.webp',
         cat: '',
         type: 'new',
     },
-
     {
         id: 6,
         Name: "Product6",
         price: '200',
-        img: 'img/p6.webp',
+        image: 'img/p6.webp',
         cat: '',
         type: 'new',
     },
-
     {
         id: 7,
         Name: "Product7",
         price: '220',
-        img: 'img/p7.webp',
+        image: 'img/p7.webp',
         cat: '',
         type: 'new',
     },
-
     {
         id: 8,
         Name: "Product8",
         price: '240',
-        img: 'img/p.webp',
+        image: 'img/p8.webp',
         cat: '',
         type: 'new',
     }
-]
+];
 
-export default Homeproduct
+export default Homeproduct;
