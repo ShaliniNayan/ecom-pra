ada171dab679bf07ac617778c8748378371dc9ea
Shalini Nayan <shalininayan@outlook.com>
2024-11-01 22:45:07 +0530

    Update

--- src/comp/home.js
+++ src/comp/home.js
@@ -59,7 +59,11 @@ const Home = () => {
               </div>
             </div>
             <div className='right_box'>
-              <div></div>
+              <div>
+                <div className="w-full p-2.5 bg-[#f6f7f8]">
+                  <div></div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
