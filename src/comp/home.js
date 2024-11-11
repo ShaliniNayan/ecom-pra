1e90763d48cae23daf9968a2256b641453762a03
Shalini Nayan <shalininayan@outlook.com>
2024-11-11 23:16:01 +0530

    Update

--- src/comp/home.js
+++ src/comp/home.js
@@ -72,10 +72,7 @@ const Home = () => {
               </div>
             </div>
             <div className='right_box'>
-              <div>
-                <div>
-                  <div></div>
-                </div>
+              <div>                
               </div>
             </div>
           </div>
