475f4cd5fc85ffd813243542bde3a4248a641fef
Shalini Nayan <shalininayan@outlook.com>
2024-11-22 17:18:59 +0530

    Update info styling

--- src/comp/home.js
+++ src/comp/home.js
@@ -85,7 +85,7 @@ const Home = () => {
                     <div className='img_box'>
                       <img className='ml-[50px]' src='image/T1.avif' alt='testimonial'></img>
                     </div>
-                    <div className='info'>
+                    <div className='info mt-[10px]'>
                       <h3>Emmanual Musangranfa</h3>
                       <h4>Web Designer</h4>
                       <p>lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
