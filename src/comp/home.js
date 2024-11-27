156e9e43672e21e10cf0ca768317ddf0b05413da
Shalini Nayan <shalininayan@outlook.com>
2024-11-27 12:42:16 +0530

    Error fixed

--- src/comp/home.js
+++ src/comp/home.js
@@ -76,7 +76,7 @@ const Home = () => {
               </div>
             </div>
             <div className='right_box font-semibold p-[10px] px-[20px] w-[24%] flex flex-col'>
-              <div className='right_container max-w-full flex'>
+              <div className='right_container max-w-full flex flex-col'>
                 <div className='testimonial w-[100%] border-4 border-[#f6f7f8]'>
                   <div className='head px-[10px] py-[10px] w-[100%] bg-#f6f7f8'>
                     <h3 className="text-center text-[#232323] text-[16px] uppercase mt-[10px] tracking-[1px]">Our Testimonial</h3>
