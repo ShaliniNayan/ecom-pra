df26b79b1084b3c91a832adbd4bdfea1bedbd546
Shalini Nayan <shalininayan@outlook.com>
2024-12-23 18:49:39 +0530

    Update the function for featured Product

--- src/comp/home.js
+++ src/comp/home.js
@@ -36,6 +36,12 @@ const Home = () => {
       return x.type === 'new';
     })
     setNewProduct(newCategory);
+
+    // Featured Product
+    const featuredCategory = Homeproduct.filter((x) => {
+      return x.type === 'featured';
+    })
+    setFeaturedProduct(featuredCategory);
   }
 
   return (
@@ -179,9 +185,9 @@ const Home = () => {
         <div className="product_type px-[20px] py-[30px] w-[100%]">
           <div className="container flex w-[100%] space-between">
             <div className="box w-[400px] h-[496px] border-3 border-[#f6f7f8] p-[10px] overflow-hidden">
-            <div className="header">
-              <h2>New Product</h2>
-            </div>
+              <div className="header">
+                <h2>New Product</h2>
+              </div>
             
             {
               newProduct.map((curElm) => {
@@ -209,7 +215,36 @@ const Home = () => {
             
             </div>
           </div>
-          
+          <div className="box w-[400px] h-[496px] border-3 border-[#f6f7f8] p-[10px] overflow-hidden">
+              <div className="header">
+                <h2>New Product</h2>
+              </div>
+            
+              {
+                featuredProduct.map((curElm) => {
+                  return(
+                    <>
+                      <div className="productbox">
+                        <div className="img_box">
+                          <img src={curElm.image} alt="" />
+                        </div>
+                        <div className="detail">
+                          <h3>{curElm.Name}</h3>
+                          <p>₹{curElm.price}</p>
+                          <div className="icon">
+                            <button><AiFillEye /></button>
+                            <button><AiFillHeart /></button>
+                            <button><AiOutlineShoppingCart /></button>
+                          </div>
+                        </div>
+                      </div> 
+                    </>
+                  )
+                  
+                })
+              }
+            
+            </div>
         </div>
       </div>
     </>
