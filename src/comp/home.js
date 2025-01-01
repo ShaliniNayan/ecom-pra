511c237d95e83a892829875a441750b131d94036
Shalini Nayan <shalininayan@outlook.com>
2025-01-01 12:15:43 +0530

    Fix styling error

--- src/comp/home.js
+++ src/comp/home.js
@@ -188,10 +188,10 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div className="product_type px-[20px] py-[30px] w-[100%] bg-[#f6f7f8]">
+        <div className="product_type px-[20px] py-[30px] w-[100%]">
           <div className="container flex flex-wrap justify-between gap-[5px]">
             {/* New Product Box */}
-            <div className="box w-[360px] h-[490px] border-3 border-[#f6f7f8] bg-[#f6f7f8]  overflow-hidden">
+            <div className="box w-[360px] h-[490px] border-3 border-[#f6f7f8] font-semibold overflow-hidden">
               <div className="header w-[100%] h-[50px] bg-[#f6f6f6] px-[10px] py-[10px]">
                 <h2 className="text-[#232323] text-[16px] uppercase font-semibold">New Product</h2>
               </div>
@@ -214,7 +214,7 @@ const Home = () => {
             </div>
 
             {/* Featured Product Box */}
-            <div className="box w-[360px] h-[490px] border-3 border-[#f6f7f8]  overflow-hidden">
+            <div className="box w-[360px] h-[490px] border-3 border-[#f6f7f8] font-semibold overflow-hidden">
               <div className="header w-[100%] h-[50px] bg-[#f6f6f6] px-[10px] py-[10px">
                 <h2 className="text-[#232323] text-[16px] uppercase font-semibold">Featured Product</h2>
               </div>
@@ -237,7 +237,7 @@ const Home = () => {
             </div>
 
             {/* Top Product Box */}
-            <div className="box w-[360px] h-[490px] border-3 border-[#f6f7f8]  overflow-hidden">
+            <div className="box w-[360px] h-[490px] border-3 border-[#f6f7f8] font-semibold overflow-hidden">
               <div className="header w-[100%] h-[50px] bg-[#f6f6f6] px-[10px] py-[10px">
                 <h2 className="text-[#232323] text-[16px] uppercase font-semibold">Top Product</h2>
               </div>
