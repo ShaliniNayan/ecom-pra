92a38ac3d94cfc22417ba0732ba840e85e4cbfa6
Shalini Nayan <shalininayan@outlook.com>
2025-01-03 11:35:31 +0530

    Add Icon of free shipping, supprt and wallet

--- src/comp/footer.js
+++ src/comp/footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './nav.css'
-import { FaPiggyBank } from 'react-icons/fa'
+import {  FaHeadphonesAlt, FaPiggyBank, FaShippingFast, FaWallet } from 'react-icons/fa'
 
 const Footer = () => {
   return (
@@ -13,7 +13,35 @@ const Footer = () => {
 							<FaPiggyBank />
 						</div>
 						<div classNmae="detail">
-							
+							<h3>Great value for your money</h3>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</div>
+					</div>
+					<div className='box'>
+						<div className="icon_box">
+							<FaShippingFast />
+						</div>
+						<div classNmae="detail">
+							<h3>Free shipping </h3>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</div>
+					</div>
+					<div className='box'>
+						<div className="icon_box">
+							<FaHeadphonesAlt />
+						</div>
+						<div classNmae="detail">
+							<h3>24X7 Support</h3>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+						</div>
+					</div>
+					<div className='box'>
+						<div className="icon_box">
+							<FaWallet />
+						</div>
+						<div classNmae="detail">
+							<h3>Money back</h3>
+							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 						</div>
 					</div>
 				</div>
