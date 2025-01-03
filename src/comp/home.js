bd020ee0f8420107a1ce1aea3bd97291d31bb613
Shalini Nayan <shalininayan@outlook.com>
2025-01-03 10:48:38 +0530

    update

--- src/comp/home.js
+++ src/comp/home.js
@@ -258,10 +258,7 @@ const Home = () => {
                 </div>
               ))}
             </div>
-            <div>
-              
-            </div>
-
+            
           </div>
         </div>
         
