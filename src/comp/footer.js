03368b4b6e52ac583d56a893b5580e20cccc18dd
Shalini Nayan <shalininayan@outlook.com>
2025-01-07 21:24:28 +0530

    Trying to fix bug for flex

--- src/comp/footer.js
+++ src/comp/footer.js
@@ -7,39 +7,39 @@ const Footer = () => {
 	<>
 		<div className="footer w-full p-8 bg-[#f6f7f8]">
 			<div className='container flex font-semibold max-w-full'>
-				<div className="left-box flex flex-col w-1/3 p-5 border-r-2 border-[#b8b7b7]">
-					<div className='box flex items-center gap-4'>
-						<div className="icon_box flex items-center justify-center w-12 h-12 text-2xl text-blue-500">
+				<div className="left-box w-1/3 p-5 border-r-2 border-[#b8b7b7]">
+					<div className='box flex px-[10px] py-[20px] mt-4 gap-4'>
+						<div className="icon_box">
 							<FaPiggyBank />
 						</div>
-						<div className="detail space-y-1">
+						<div className="detail">
 							<h3>Great value for your money</h3>
 							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 						</div>
 					</div>
 					<div className='box flex items-center gap-4'>
-						<div className="icon_box flex items-center justify-center w-12 h-12 text-2xl text-blue-500">
+						<div className="icon_box">
 							<FaShippingFast />
 						</div>
-						<div className="detail space-y-1">
+						<div className="detail">
 							<h3>Free shipping </h3>
 							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 						</div>
 					</div>
 					<div className='box flex items-center gap-4'>
-						<div className="icon_box flex items-center justify-center w-12 h-12 text-2xl text-blue-500">
+						<div className="icon_box">
 							<FaHeadphonesAlt />
 						</div>
-						<div className="detail space-y-1">
+						<div className="detail">
 							<h3>24X7 Support</h3>
 							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 						</div>
 					</div>
 					<div className='box flex items-center gap-4'>
-						<div className="icon_box flex items-center justify-center w-12 h-12 text-2xl text-blue-500">
+						<div className="icon_box">
 							<FaWallet />
 						</div>
-						<div className="detail space-y-1">
+						<div className="detail">
 							<h3>Money back</h3>
 							<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 						</div>
