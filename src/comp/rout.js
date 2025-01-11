a656f9c2ff076ae8b6bf71082fb7fca4e4c9bbe5
Shalini Nayan <shalininayan@outlook.com>
2025-01-11 22:14:42 +0530

    Update the route for shop

--- src/comp/rout.js
+++ src/comp/rout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './home'
+import Shop from './shop'
 const Rout = () => {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />}/>
+        <Route path='shop' element={<Shop />}/>
       </Routes>
     </>
   )
