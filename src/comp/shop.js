ace0d1b242db0828bb5f714af2929a0003e7191e
Shalini Nayan <shalininayan@outlook.com>
2025-01-12 21:24:46 +0530

    Update styling for shop

--- src/comp/shop.js
+++ src/comp/shop.js
@@ -4,7 +4,7 @@ import './nav.css';
 const Shop = () => {
     return (
         <>
-            <div className="shop">
+            <div className="shop w-[100px] px-8 py-5">
                 <div className="container">
                     <div className="left_box">
 
