5ec4b09ef9c17e8a8c89c42221243f518d71d961
Shalini Nayan <shalininayan@outlook.com>
2025-01-16 20:20:44 +0530

    Update styling category

--- src/comp/shop.js
+++ src/comp/shop.js
@@ -10,10 +10,10 @@ const Shop = () => {
                 </h2>
                 <p className="whitespace-nowrap mt-1 text-[#8a8a8a] text-sm font-semibold">Home  shop</p>
                 <div className="container w-full flex mt-[20px]">
-                    <div className="left_box w-[30%] flex">
-                        <div className="category border-[3px] border-[#f6f6f6]">
+                    <div className="left_box w-[25%] flex">
+                        <div className="category border-[3px] border-[#f6f6f6] w-full">
                             <div className="header flex">
-                                <h2 className="flex font-semibold whitespace-nowrap uppercase text-[#232323] text-xl">All Categories</h2>
+                                <h2 className="flex font-semibold whitespace-nowrap uppercase text-[#232323] text-2xl">All Categories</h2>
                             </div>
                             <div className="box">
                                 <ul className="list-disc pl-5 space-y-2">
