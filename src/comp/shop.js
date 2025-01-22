ec5477628b08e130fb26a2da5419c8998c77a324
Shalini Nayan <shalininayan@outlook.com>
2025-01-22 08:22:06 +0530

    Add right box banner image

--- src/comp/shop.js
+++ src/comp/shop.js
@@ -45,6 +45,15 @@ const Shop = () => {
                         </div>
                     </div>
                 </div>
+
+                {/* Right Box */}
+                <div className="right_box w-3/4">
+                    <div className="banner">
+                        <div className="img_box">
+                            <img src="image/shop_top.webp" alt="" className="w-full object-cover" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
