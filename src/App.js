72f47be940cba5c055ee7a65f685bb3df28afa0b
Shalini Nayan <shalininayan@outlook.com>
2025-01-24 17:19:08 +0530

    Add shop fproduct page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from './comp/nav'
 import { BrowserRouter } from 'react-router-dom'
 import Rout from './comp/rout'
 import Footer from './comp/footer'
+import Homeproduct from './comp/home_product'
 const App = () => {
+  // Shop Page Product
+  const [shop, setShop] = useState(Homeproduct)
   return (
     <>
       <BrowserRouter>
         <Nav />
-        <Rout />
+        <Rout shop={shop} />
         <Footer />
       </BrowserRouter>
     </>
