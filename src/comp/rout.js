b79658f046040a51b48f2ba3f66ddc47718ca7a9
Shalini Nayan <shalininayan@outlook.com>
2025-02-04 23:29:16 +0530

    Add and update cart page

--- src/comp/rout.js
+++ src/comp/rout.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './home'
 import Shop from './shop'
+import Cart from './cart'
 const Rout = ({shop, Filter, allcatefilter}) => {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='shop' element={<Shop shop={shop} Filter={Filter} allcatefilter={allcatefilter} />}/>
+        <Route path='cart' element={<Cart />}/>
       </Routes>
     </>
   )
