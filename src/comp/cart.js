d5588e04d87c354d1b4c58ca22c356bfa6e430ed
Shalini Nayan <shalininayan@outlook.com>
2025-02-05 23:40:15 +0530

    Update function of cart and add shop now button

--- src/comp/cart.js
+++ src/comp/cart.js
@@ -1,11 +1,20 @@
 import React from "react";
 import './nav.css';
 
-const Cart = () => {
+const Cart = ({cart}) => {
     return (
        <>
         <div className="cart w-full px-[20px] py-[30px]">
             <h3 className="text-2xl uppercase text-[#232323] tracking-[1px] font-semibold">#cart</h3>
+            {
+                cart.length === 0 && 
+                <>
+                <div className="empty_cart">
+                    <h2>Your Cart is Empty</h2>
+                    <button>Shop Now</button>
+                </div>
+                </>
+            }
             <div className="conatiner"></div>
         </div>
        </>
