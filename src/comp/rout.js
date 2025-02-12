bb980d56071e281ed61c17f25c8d9f4be700cf4e
Shalini Nayan <shalininayan@outlook.com>
2025-02-12 22:14:39 +0530

    Update route for set cart

--- src/comp/rout.js
+++ src/comp/rout.js
@@ -3,13 +3,13 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './home'
 import Shop from './shop'
 import Cart from './cart'
-const Rout = ({cart, shop, Filter, allcatefilter, addtocart}) => {
+const Rout = ({cart, setCart, shop, Filter, allcatefilter, addtocart}) => {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='shop' element={<Shop shop={shop} Filter={Filter} allcatefilter={allcatefilter} addtocart={addtocart} />}/>
-        <Route path='cart' element={<Cart cart={cart} />}/>
+        <Route path='cart' element={<Cart cart={cart} setCart={setCart} />}/>
       </Routes>
     </>
   )
