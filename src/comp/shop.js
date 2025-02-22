3cd712b2d8936b7e96e77e1ecef04d225bb57c8b
Shalini Nayan <shalininayan@outlook.com>
2025-02-22 23:27:02 +0530

    Update styling for image

--- src/comp/shop.js
+++ src/comp/shop.js
@@ -34,7 +34,8 @@ const Shop = ({shop, Filter, allcatefilter, addtocart}) => {
                 </button>
                 <div className="container w-full flex">
                     <div className="img_box px-[20px] py-[20px] bg-[#f6f6f6] rounded-md">
-                        <img src={detail.image} alt="" />
+                        <img className="w-[270px] h-[270px] " 
+                        src={detail.image} alt="" />
                     </div>
                 </div>
             </div>
