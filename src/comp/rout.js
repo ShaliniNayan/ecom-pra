ebd0687c9a3f67cb2fab7c611d5ffe97ed846dd1
Shalini Nayan <shalininayan@outlook.com>
2025-02-25 22:51:35 +0530

    Update contact path

--- src/comp/rout.js
+++ src/comp/rout.js
@@ -3,13 +3,15 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './home'
 import Shop from './shop'
 import Cart from './cart'
+import Contact from './contact'
 const Rout = ({cart, setCart, shop, Filter, allcatefilter, addtocart}) => {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home addtocart={addtocart} />}/>
         <Route path='shop' element={<Shop shop={shop} Filter={Filter} allcatefilter={allcatefilter} addtocart={addtocart} />}/>
-        <Route path='cart' element={<Cart cart={cart} setCart={setCart} />}/>
+        <Route path='/cart' element={<Cart cart={cart} setCart={setCart} />}/>
+        <Route path='/contact' element={<Contact />}/>
       </Routes>
     </>
   )
