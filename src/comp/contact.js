37a2057e374cc45a2052231101de935624dd7748
Shalini Nayan <shalininayan@outlook.com>
2025-02-26 19:20:04 +0530

    Add other input regarding message

--- src/comp/contact.js
+++ src/comp/contact.js
@@ -17,6 +17,31 @@ const Contact = () => {
                                 <input type="text" placeholder="Enter your name" value="" name="" />
                             </div>
                         </div>
+                        <div className="box">
+                            <div className="label">
+                                <h4>Email</h4>
+                            </div>
+                            <div className="input">
+                                <input type="email" placeholder="Enter your email" value="" name="" />
+                            </div>
+                        </div>
+                        <div className="box">
+                            <div className="label">
+                                <h4>Subject</h4>
+                            </div>
+                            <div className="input">
+                                <input type="text" placeholder="Enter your subject" value="" name="" />
+                            </div>
+                        </div>
+                        <div className="box">
+                            <div className="label">
+                                <h4>Message</h4>
+                            </div>
+                            <div className="input">
+                                <textarea placeholder="Enter your message !" value="" name=""></textarea>
+                            </div>
+                        </div>
+                        
                     </form>
                 </div>
             </div>
