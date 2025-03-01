a18ff8ca751a37f9c355ee65eca84add046b7b59
Shalini Nayan <shalininayan@outlook.com>
2025-03-01 14:43:45 +0530

    Update the path for payment

--- src/comp/rout.js
+++ src/comp/rout.js
@@ -4,6 +4,7 @@ import Home from './home'
 import Shop from './shop'
 import Cart from './cart'
 import Contact from './contact'
+import Payment from './payment'
 const Rout = ({cart, setCart, shop, Filter, allcatefilter, addtocart}) => {
   return (
     <>
@@ -12,6 +13,7 @@ const Rout = ({cart, setCart, shop, Filter, allcatefilter, addtocart}) => {
         <Route path='shop' element={<Shop shop={shop} Filter={Filter} allcatefilter={allcatefilter} addtocart={addtocart} />}/>
         <Route path='/cart' element={<Cart cart={cart} setCart={setCart} />}/>
         <Route path='/contact' element={<Contact />}/>
+        <Route path='/payment' element={<Payment />}/>
       </Routes>
     </>
   )
